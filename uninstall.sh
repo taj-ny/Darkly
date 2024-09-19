f0dfdade6422076a5e92c491c2a60f6f2f8be166
Séra Balázs <serabali10050@gmail.com>
2024-09-19 20:06:39 +0200

    Added a very basic uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm /usr/share/color-schemes/Lightly.colors
+rm /usr/lib/qt6/plugins/styles/lightly6.so
+rm /usr/share/kstyle/themes/lightly.themerc
+rm /usr/lib/qt6/plugins/kstyle_config/lightlystyleconfig.so
+rm /usr/share/applications/lightlystyleconfig.desktop
+rm /usr/bin/lightly-settings6
+rm /usr/share/icons/hicolor/scalable/apps/lightly-settings.svgz
+rm /usr/lib/liblightlycommon6.so.6.80
+rm /usr/lib/liblightlycommon6.so.6
+rm /usr/lib/qt6/plugins/org.kde.kdecoration2/org.kde.lightly.so
+rm /usr/share/kservices6/lightlydecorationconfig.desktop
+rm /usr/lib/qt6/plugins/org.kde.kdecoration2.kcm/kcm_lightlydecoration.so
+rm /usr/share/applications/kcm_lightlydecoration.desktop
+rm /usr/lib/cmake/Lightly/LightlyConfig.cmake
+rm /usr/lib/cmake/Lightly/LightlyConfigVersion.cmake
+rmdir /usr/lib/cmake/Lightly
